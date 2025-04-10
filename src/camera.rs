ead352ac7ae76104ec461b4658b6bc531553c3aa
IR25721 <zunik0421@gmail.com>
2025-04-11 08:57:16 +0900

    move camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,15 +1,23 @@
 use bevy::prelude::*;
 
 pub fn setup_camera(mut commands: Commands) {
-    commands.spawn((Camera2dBundle {
-        camera: Camera {
-            viewport: Some(Viewport {
-                physical_position: UVec2::new(0, 0),
-                physical_size: UVec2::new(256, 256),
-                ..default()
-            }),
-            ..default()
-        },
-        ..default()
-    },));
+    commands.spawn(Camera2d);
+}
+pub fn move_camera_with_player(
+    mut camera: Query<&mut Transform, With<Camera2d>>,
+    player: Query<&Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    let Ok(mut camera) = camera.get_single_mut() else {
+        return;
+    };
+    let Ok(player) = player.get_sigle_mut() else {
+        return;
+    };
+
+    Vec3 { x, y, .. } = player.transform;
+    let direction = Vec3::new(x, y, camera.translation.z);
+    camera
+        .translation
+        .smooth_nudge(&direction, 2., time.delta_secs());
 }
