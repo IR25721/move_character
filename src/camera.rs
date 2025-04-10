1c83bbd677eaf2697ff11f6eda3f9b63ad409a33
IR25721 <zunik0421@gmail.com>
2025-04-11 06:12:24 +0900

    change_camera_viewport

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,15 @@
 use bevy::prelude::*;
 
 pub fn setup_camera(mut commands: Commands) {
-    commands.spawn((Camera2d,));
+    commands.spawn((Camera2dBundle {
+        camera: Camera {
+            viewport: Some(Viewport {
+                physical_position: UVec2::new(0, 0),
+                physical_size: UVec2::new(256, 256),
+                ..default()
+            }),
+            ..default()
+        },
+        ..default()
+    },));
 }
