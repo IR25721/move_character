2a3b170750a1f14ac4ed3485e410f4cd4cbea973
IR25721 <zunik0421@gmail.com>
2025-04-11 10:46:25 +0900

    add run

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ use character::setup_character;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (setup_character, setup_camera));
+        .add_systems(Startup, (setup_character, setup_camera))
+        .run();
 }
