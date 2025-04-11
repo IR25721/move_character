61e99eb79586f99ebb8b13a23deb32569f562def
IR25721 <zunik0421@gmail.com>
2025-04-11 14:13:52 +0900

    update

--- src/character.rs
+++ src/character.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 const PLAYER_SPEED: f32 = 5.;
 #[derive(Component)]
-struct Player;
+pub struct Player;
 pub fn setup_character(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -10,22 +10,22 @@ pub fn setup_character(
     let texture_handle: Handle<Image> = asset_server.load("woman_walking.png");
     let texture_atlas_layout = TextureAtlasLayout::from_grid(UVec2::new(24, 24), 6, 4, None, None);
     let texture_atlas_layout_handle = texture_atlases.add(texture_atlas_layout);
-    commands.spawn(Sprite::from_atlas_image(
+    commands.spawn((Player,Sprite::from_atlas_image(
         texture_handle,
         TextureAtlas {
             layout: texture_atlas_layout_handle,
             index: 0,
         },
-    ));
+    )));
 }
 pub fn move_player(
     mut player: Query<&mut Transform, With<Player>>,
-    time: Res<time>,
+    time: Res<Time>,
     kb_input: Res<ButtonInput<KeyCode>>,
 ) {
     let Ok(mut player) = player.get_single_mut() else {
         return;
-
+    };
         let mut direction = Vec2::ZERO;
 
         if kb_input.pressed(KeyCode::KeyW) {
@@ -40,7 +40,6 @@ pub fn move_player(
         if kb_input.pressed(KeyCode::KeyD) {
             direction.x += 1.;
         }
-    };
     let move_delta = direction.normalize_or_zero() * PLAYER_SPEED * time.delta_secs();
     player.translation += move_delta.extend(0.);
 }
